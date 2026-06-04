61f131d297a989d31d4dd37e5ba5dead8ba770c0
GeQiuyang <geqiuyang1@gmail.com>
2026-06-04 10:25:47 +0800

    refactor: remove footer from all pages

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue'
 import TopNavBar from '@/components/layout/TopNavBar.vue'
 import SideNavBar from '@/components/layout/SideNavBar.vue'
-import FooterBar from '@/components/layout/FooterBar.vue'
 
 const sidebarVisible = ref(false)
 
@@ -21,7 +20,5 @@ function closeSidebar() {
     <SideNavBar :visible="sidebarVisible" @close="closeSidebar" />
     <main class="md:ml-64 pt-20 pb-16 px-6 max-w-[1440px] mx-auto min-h-screen">
       <RouterView />
-    </main>
-    <FooterBar class="md:ml-64" />
-  </div>
+    </main>  </div>
 </template>
